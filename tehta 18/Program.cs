ba1e87efde013421941dddd498248f5cf3cb5895
Olli <Olli@OLLI-PC>
2017-01-26 11:48:14 +0200

    18

---
+++ tehta 18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna merkkijono > ");
+            var original = Console.ReadLine();
+            var reversed = new string(original.Reverse().ToArray());
+            Console.WriteLine();
+
+
+            Console.WriteLine(original);
+            Console.WriteLine(reversed);
+            if (original == reversed)
+            { Console.WriteLine("Palidromi"); }
+            else
+            { Console.WriteLine("Ei palidromi"); }
+
+            Console.ReadLine();
+        }
+    }
+}
