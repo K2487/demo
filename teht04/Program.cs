f5de25abd79fec6aa151d9857c68f93cd8a1801b
Olli <Olli@OLLI-PC>
2017-01-26 09:41:51 +0200

    21

--- teht04/Program.cs
+++ teht04/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace teht04
+namespace teht4
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int ika;
+
+            Console.WriteLine("Anna ikäsi >");
+            string line = Console.ReadLine();
+            ika = int.Parse(line);
+
+            if (ika < 18)
+            {
+                Console.WriteLine("Alaikäinen");
+            }
+            else if (ika >= 18 && ika <= 65)
+            {
+                Console.WriteLine("Aikuinen");
+            }
+            else if (ika > 65)
+            {
+                Console.WriteLine("Seniori");
+            }
         }
     }
 }
