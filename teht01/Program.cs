f832335f9f0d25d892794d797d5ef0216041808a
Olli <Olli@OLLI-PC>
2017-01-26 10:07:07 +0200

    01

---
+++ teht01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // kysyy luvun
+            int luku;
+            Console.Write("Anna luku >");
+            string line = Console.ReadLine(); 
+            luku = int.Parse(line); 
+            // numero
+            switch (luku)
+            {
+                case 1:
+                    Console.WriteLine("yksi");
+                    break;
+
+                case 2:
+                    Console.WriteLine("kaksi");
+                    break;
+
+                case 3:
+                    Console.WriteLine("kolme");
+                    break;
+
+                default:
+                    Console.WriteLine("Joku muu luku");
+                    break;
+            }
+        
+            Console.ReadLine();
+        }
+    }
+}
